cc85022d0e7da5b149b35eccf22177575185daad
Marijn <marijn.sopers@Hotmail.nl>
2024-05-31 14:10:08 +0200

    simple implementation webots connection

--- ASWebotsV2/controllers/pickup_robot_0/build/release/pickup_robot_0.d
+++
@@ -1,9 +0,0 @@
-build/release/pickup_robot_0.o: pickup_robot_0.cpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Supervisor.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Program\ Files/Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Node.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Field.hpp \
- C:/Program\ Files/Webots/include/controller/c/webots/contact_point.h \
- C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp
--- ASWebotsV2/controllers/pickup_robot_2/build/release/pickup_robot_2.d
+++
@@ -1,9 +0,0 @@
-build/release/pickup_robot_2.o: pickup_robot_2.cpp \
- C:\Users\clare\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Users\clare\AppData\Local\Programs\Webots/include/controller/cpp/webots/Supervisor.hpp \
- C:\Users\clare\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Users/clare/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
- C:\Users\clare\AppData\Local\Programs\Webots/include/controller/cpp/webots/Node.hpp \
- C:\Users\clare\AppData\Local\Programs\Webots/include/controller/cpp/webots/Field.hpp \
- C:/Users/clare/AppData/Local/Programs/Webots/include/controller/c/webots/contact_point.h \
- C:\Users\clare\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp
